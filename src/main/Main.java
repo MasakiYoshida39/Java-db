fc843e2e56bfc122cc0d1ed0c3423a3db22934a0
MasakiYoshida39 <yashukasono@gmail.com>
2025-06-12 12:24:54 +0900

    登録可能に変更

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,20 +1,21 @@
 package main;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Types;
 import java.util.Scanner;
 
 
 public class Main {
-
 	/**
 	 * キーボード入力用のScannerオブジェクト（インスタンス）
 	 */
-	private static final Scanner scan = new Scanner(System.in);
+	private static final Scanner scan = new Scanner(System.in);;
 
 	/**
 	 * メインメソッド
@@ -22,9 +23,15 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		displayList();			// 一覧表示
+		displayList();				// 一覧表示
 
-		select();				// 検索
+		String userId = insert();	// 登録
+		
+		if (userId != null) {
+			select(userId);			// 検索
+		}
+		select1();				// 検索
+	
 	}
 
 	/**
@@ -49,7 +56,7 @@ public class Main {
 
 		String sql = "SELECT user_id FROM m_user ";
 
-		System.out.println("\n---全てのユーザID---\n");
+		System.out.println("\n---全てのユーザ---\n");
 		try (Connection con = DriverManager.getConnection(url, user, password);
 				Statement stmt = con.createStatement();
 				ResultSet res = stmt.executeQuery(sql)) {
@@ -63,9 +70,104 @@ public class Main {
 	}
 	
 	/**
-	 * ユーザIDの入力を受け付け、１件のユーザデータの検索表示
+	 * ユーザデータの入力を受付けて登録する。
+	 * @return 登録した ユーザID (null の場合 登録NG）
 	 */
-	private static void select() {
+	private static String insert() {
+
+		String url = "jdbc:mysql://localhost:3306/pc_shop_db";
+		String user = "root";
+		String password = "";
+
+		String sql =  " INSERT INTO m_user "
+			     	+ " (user_id, user_name, email, birth_day)"
+			     	+ " VALUES (?, ?, ?, ?) ";
+
+		System.out.println("\n---登録します---\n");
+
+		// 登録データの入力を受け付ける
+		String userId 			= inputPrompt("【Ｉ　Ｄ】 > ");
+		String userName 		= inputPrompt("【名　前】 > ");
+		String email 			= inputPrompt("【メール】 > ");
+		String birthDayString 	= inputPrompt("【誕生日】 > ");
+		// birthDay (java.sql.Date型)の設定
+		// （誕生日入力が 空文字の場合 null、
+		//    入力文字列がある場合 java.sql.Dateに変換して設定）
+		Date birthDay = null;
+		if (! birthDayString.equals("")) {
+			try {
+				birthDay = Date.valueOf(birthDayString);
+			} catch (IllegalArgumentException e) {
+				System.out.println("\n日付は yyyy-mm-dd で指定してください。");
+				return null;
+			}
+		}
+
+		// SQL の実行
+		try (Connection con = DriverManager.getConnection(url, user, password);
+				PreparedStatement pstmt = con.prepareStatement(sql)) {
+			// プレースホルダに値を設定
+			pstmt.setString(1, userId);
+			pstmt.setString(2, userName);
+			pstmt.setString(3, email);
+			if (birthDay == null) {
+				pstmt.setNull(4, Types.DATE);
+			} else {
+				pstmt.setDate(4, birthDay);
+			}
+
+			int cnt = pstmt.executeUpdate();	// 登録処理の実行
+			
+			System.out.println("\n"+ cnt + "件 登録しました。");
+
+		} catch (SQLException e) {
+			System.out.println("\nエラーが発生しました。");
+			return null;
+		}
+	
+		return userId;	// 登録した ユーザID を返す
+	}
+
+	/**
+	 * 指定された １件のユーザデータの検索表示
+	 * @param userId ユーザID
+	 */
+	private static void select(String userId) {
+
+		String url = "jdbc:mysql://localhost:3306/pc_shop_db";
+		String user = "root";
+		String password = "";
+
+		String sql = "SELECT * FROM m_user WHERE user_id = ? ";
+		
+		try (Connection db = DriverManager.getConnection(url, user, password);
+				PreparedStatement pstmt = db.prepareStatement(sql)) {
+			
+			pstmt.setString(1, userId);		// プレースホルダに値を設定
+			
+			try (ResultSet res = pstmt.executeQuery()) {	// 検索の実行
+				if (res.next()) {
+					// 検索結果が存在する場合に内容表示
+					System.out.println("\n---検索結果---\n");
+					System.out.println("【Ｉ　Ｄ】" + res.getString("user_id"));
+					System.out.println("【名　前】" + res.getString("user_name"));
+					System.out.println("【メール】" + res.getString("email"));
+					if (res.getDate("birth_day") == null) {
+						System.out.println("【誕生日】（未登録）");
+					} else {
+						System.out.println("【誕生日】" + res.getDate("birth_day"));
+					}
+				} else {
+					System.out.println("\nレコードが存在しません。");
+				}
+			}
+		} catch (SQLException e) {
+			System.out.println("\nエラーが発生しました。");
+		}
+	}
+	
+	
+	private static void select1() {
 
 		String url = "jdbc:mysql://localhost:3306/pc_shop_db";
 		String user = "root";
@@ -103,6 +205,7 @@ public class Main {
 	}
 
 }
+
 
 
 
